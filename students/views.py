67b62d8870e581d788f1646d4d9e2ce53a4ec60a
OREOLUWA-NIMI2025 <oladimejioreoluwa070@gmail.com>
2025-09-03 16:09:06 +0100

    frist commit

--- students/views.py
+++ students/views.py
@@ -3,32 +3,32 @@ from rest_framework.decorators import api_view
 from .serializers import StudentSerializer, CourseSerializer
 from .models import  Student, Course
 from rest_framework.response import Response
-
+from rest_framework import status
 
 @api_view(["GET"])
 def list_students(request):
     students = Student.objects.all()
     serializer = StudentSerializer(students, many=True)
-    return Response(serializer.data)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
 @api_view(["GET"])
 def list_courses(request):
     courses = Course.objects.all()
     serializer = CourseSerializer(courses, many=True)
-    return Response(serializer.data)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
 @api_view(["GET"])
 def retreive_student(request, id):
     student = Student.objects.get(id=id)
     # student = get_object_or_404(student, id=id)
     serializer = StudentSerializer(student)
-    return Response(serializer.data)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
 @api_view(["GET"])
 def retreive_course(request, id):
     course = Course.objects.get(id=id)
     serializer = CourseSerializer(course)
-    return Response(serializer.data)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 @api_view(["POST"])
@@ -36,18 +36,18 @@ def create_students(request):
     serializer = StudentSerializer(data=request.data)
     if serializer.is_valid():
       serializer.save()
-      return Response(serializer.data)
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
     else:
-        return Response(serializer.errors)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(["POST"])   
 def create_courses(request):
     serializer = CourseSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
     else:
-        return Response(serializer.errors)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 @api_view(["PUT"])
 def update_student(request, id):
@@ -55,9 +55,9 @@ def update_student(request, id):
     serializer = StudentSerializer(student , data=request.data, partial=True)
     if serializer.is_valid():
         serializer.save()
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     else:
-        return Response(serializer.errors)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(["PUT"])  
 def update_course(request, id):
@@ -65,18 +65,18 @@ def update_course(request, id):
     serializer = CourseSerializer(course, data=request.data, partial=True)
     if serializer.is_valid():
         serializer.save()
-        return Response(serializer.data)
+        return Response(serializer.data,status=status.HTTP_200_OK)
     else:
-        return Response(serializer.errors)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(["DELETE"])
 def delete_student(request, id):
     student=get_object_or_404(Student, id=id)
     student.delete()
-    return Response("student-delete-successfully")
+    return Response("student-delete-successfully", status=status.HTTP_200_OK)
 
 @api_view(["DELETE"])
 def delete_course(request, id):
     course=get_object_or_404(Course, id=id)
     course.delete()
-    return Response("Course-remove-successfully")
+    return Response("Course-remove-successfully", status=status.HTTP_200_OK)
